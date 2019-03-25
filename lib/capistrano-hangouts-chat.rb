42796f80749b1134abc56cff93b05278228db2f1
Tomáš Hruška <hruska@btl.cz>
2019-03-25 10:48:50 +0100

    Fix

--- lib/capistrano-hangouts-chat.rb
+++
@@ -1,1 +0,0 @@
-require 'capistrano/hangouts-chat/version'
--- lib/capistrano/hangouts_chat.rb
+++ lib/capistrano/hangouts_chat.rb
@@ -1,3 +1,1 @@
-require 'capistrano/hangouts-chat/hangouts_chat_notifier'
-
-load File.expand_path('../capistrano/tasks/hangouts_chat.rake', __FILE__)
+load File.expand_path('../tasks/hangouts_chat.cap', __FILE__)
